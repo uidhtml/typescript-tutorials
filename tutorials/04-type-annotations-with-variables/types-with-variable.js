1956de0f20c966c7d037badac2a92b33cd3ac88e
uidhtml <54753484+uidhtml@users.noreply.github.com>
2025-10-17 03:01:49 +0530

    Added chapter 4

---
+++ tutorials/04-type-annotations-with-variables/types-with-variable.js
@@ -0,0 +1,8 @@
+var fruit = "Apple";
+var count = 20;
+var hasNumber = true;
+var nothing = null;
+var notAvailable = undefined;
+notAvailable = 20;
+var date = new Date();
+console.log(date.getDate());
---
+++ tutorials/04-type-annotations-with-variables/types-with-variable.ts
@@ -0,0 +1,10 @@
+let fruit: string = "Apple";
+let count: number = 20;
+let hasNumber = true;
+
+let nothing: null = null;
+let notAvailable: undefined | number = undefined;
+notAvailable = 20;
+
+let date: Date = new Date();
+console.log(date.getDate());
