2a9fa91129c761bfeb6c359e2b01b5179c8308e2
uidhtml <54753484+uidhtml@users.noreply.github.com>
2025-11-01 11:08:21 +0530

    Added chapter 5

---
+++ tutorials/05-type-annotations-with-objects/types-with-object.ts
@@ -0,0 +1,23 @@
+//Welcome to uidhtml 🙏
+
+//Array
+let colorsArray: string[] = ["red", "green", "#cc0000"];
+let numArray: number[] = [1, 2, 3, 4, 5, 6, 7];
+let boolArray: boolean[] = [true, false, true];
+
+//class
+class Animal {
+  defaultColor = "red";
+}
+
+let animal: Animal = new Animal();
+
+//Object literals
+interface Obj {
+  x: number;
+  y: string;
+}
+let obj: Obj = {
+  x: 20,
+  y: "Gautam",
+};
